ae4fb7139c2a7ab9694a0b07c61d54df1ea299e1
laycher999 <laycher@Mail.ru>
2026-05-17 17:08:11 +0700

    Added User endpoint

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Run
+from .models import Run, User
+
 
 class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name']
--- app_run/views.py
+++ app_run/views.py
@@ -5,12 +5,23 @@ from rest_framework import status
 
 from django.conf import settings
 
-from .serializers import RunSerializer
-from .models import Run
+from .serializers import RunSerializer, UserSerializer
+from .models import Run, User
 
 class RunViewSet(viewsets.ModelViewSet):
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset
+        type = self.request.query_params.get('Type', None)
+        if type:
+            qs = qs.filter(is_staff=True)
+        return qs
 
 @api_view(['GET'])
 def company_details(request):
