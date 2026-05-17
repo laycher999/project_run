fce9b6ccce688c9e5efd5d0366f5529180b58c2c
laycher999 <laycher@Mail.ru>
2026-05-17 08:42:47 +0700

    First endpoint created - company_details

--- app_run/views.py
+++ app_run/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.conf import settings
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from django.conf import settings
+
+
+
+
+@api_view(['GET'])
+def company_details(request):
+    return Response(
+        {"company_name": settings.COMPANY_NAME,
+                     "slogan": settings.SLOGAN,
+                     "contacts": settings.CONTACTS}, status=status.HTTP_200_OK)
--- project_run/settings/base.py
+++ project_run/settings/base.py
@@ -114,3 +114,7 @@ STATIC_ROOT = 'static'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPANY_NAME = "Бегунок"
+SLOGAN = "Меньше страха - больше бега!"
+CONTACTS = "город Краснознаменск, улица Связистов 5"
