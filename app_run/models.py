8bbfbf03dd49deace859528fa2c9a64edf7f0c5d
laycher999 <laycher@Mail.ru>
2026-05-22 11:35:21 +0700

    Finished view, serializer for CollectibleItem Created validator form upload_file

--- app_run/models.py
+++ app_run/models.py
@@ -42,5 +42,5 @@ class CollectibleItem(models.Model):
 
 
 class UploadFile(models.Model):
-    file = models.FileField(upload_to='/tmp/')
+    file = models.FileField(upload_to='/tmp')
     failed = models.TextField(null=True)
